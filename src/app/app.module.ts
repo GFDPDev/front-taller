2f085aa266a8396652604d9c80bf7be3df09b1d1
FMarcelFC <fmarcelfc@gmail.com>
2025-11-03 11:57:17 -0600

    Update angular to 16

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
@@ -14,26 +14,19 @@ import { NotFoundsComponent } from './components/not-founds/not-founds.component
 import { ReporteTablaComponent } from './components/dashboard-admin/reportes/reporte-tabla/reporte-tabla.component';
 import { ComprobanteComponent } from './components/comprobante/comprobante.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    LogInComponent,
-    FacturaComponent,
-    NotFoundsComponent,
-    ReporteTablaComponent,
-    ComprobanteComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    AngularMaterialModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-  ],
-  bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
-
-})
+@NgModule({ declarations: [
+        AppComponent,
+        LogInComponent,
+        FacturaComponent,
+        NotFoundsComponent,
+        ReporteTablaComponent,
+        ComprobanteComponent
+    ],
+    bootstrap: [AppComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA], imports: [BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        AngularMaterialModule,
+        FormsModule,
+        ReactiveFormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
--- src/app/services/main.service.ts
+++ src/app/services/main.service.ts
@@ -1,10 +1,5 @@
 import { Injectable, NgZone } from '@angular/core';
-import {
-  HttpClient,
-  HttpClientModule,
-  HttpHeaders,
-  HttpResponse,
-} from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import { Res } from '../interfaces/response';
